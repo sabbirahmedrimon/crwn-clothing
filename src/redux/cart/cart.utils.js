0d1fdd960818ae2477809eb931bd6393631cf685
Md Sabbir Ahmed <sabbirtum@gmail.com>
2020-04-11 23:53:44 +0200

    updating our application with reselect, replaced cart selectors with memoized selectors

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,14 +1,15 @@
-export const addItemToCart = (carItems, cartItemToAdd) => {
-  const existingCartItem = cartItem.finds(
-    (cartItem) => carItem.id === cartItemToAdd.id
-  );
-
-  if (existingCartItem) {
-    return cartItems.map((cartItem) =>
-      carItem.id === cartItemToAdd.id
-        ? { ...carItem, quantity: carItem.quantity + 1 }
-        : cartItem
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
     );
-  }
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-};
+  
+    if (existingCartItem) {
+      return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id
+          ? { ...cartItem, quantity: cartItem.quantity + 1 }
+          : cartItem
+      );
+    }
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
